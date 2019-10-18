aea048d7f23e12e4cc39c5d15d201f2455f083eb
Geoff Davis <geoff@email2davis.co.uk>
2019-10-18 04:27:28 +0100

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -21,8 +16,6 @@ namespace API
     {
       Configuration = configuration;
     }
-
-    readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
     public IConfiguration Configuration { get; }
 
@@ -42,6 +35,8 @@ namespace API
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+
+      services.AddMediatR(typeof(List.Handler).Assembly);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
